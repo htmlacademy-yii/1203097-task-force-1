628665b0df9b82f6204aaa766a5c8a3aba4695b4
derpartizanen <skomarov@Sergeys-MacBook-Pro.local>
2019-11-28 17:45:45 +0300

    мелкие правки

--- frontend/controllers/TasksController.php
+++ frontend/controllers/TasksController.php
@@ -1,24 +1,8 @@
 <?php
 namespace frontend\controllers;
 
-use frontend\models\ResendVerificationEmailForm;
-use frontend\models\VerifyEmailForm;
-use Yii;
-use yii\base\InvalidArgumentException;
-use yii\web\BadRequestHttpException;
 use yii\web\Controller;
-use yii\filters\VerbFilter;
-use yii\filters\AccessControl;
-use common\models\LoginForm;
-use frontend\models\PasswordResetRequestForm;
-use frontend\models\ResetPasswordForm;
-use frontend\models\SignupForm;
-use frontend\models\ContactForm;
-use yii\db\Query;
-use frontend\models\Users;
-use frontend\models\UserProfiles;
 use frontend\models\Tasks;
-use frontend\models\Categories;
 
 class TasksController extends Controller
 {
--- frontend/views/tasks/index.php
+++ frontend/views/tasks/index.php
@@ -1,6 +1,9 @@
 <?php
 
-/* @var $this yii\web\View */
+/**
+ * @var $this yii\web\View
+ * @var $tasks
+ */
 
 use yii\helpers\Html;
 
